0dcadeee282c3a599cb8bd543ddf2de0ed686bfb
Vincent <vvimt@MacBook-Pro-de-Vincent.local>
2018-06-02 19:54:52 +0200

    permutations

--- comb_k_parmis_n.py
+++ comb_k_parmis_n.py
@@ -1,23 +1,23 @@
 
 def comb(k, n):
-   l_masseur = []
+   l_elem = []
    i = 0
    while i < k:
-      l_masseur.append(1)
+      l_elem.append(1)
       i = i + 1
    i = 0
-   while l_masseur[k - 1] <= n:
-      if l_masseur[0] <= n:
-         print("combinaison num " + str(int(i)) + " = " + str(l_masseur) + "\n")
+   while l_elem[k - 1] <= n:
+      if l_elem[0] <= n:
+         print("combinaison num " + str(int(i)) + " = " + str(l_elem) + "\n")
       index = 0
       while index < k - 1:
-         if l_masseur[index] > n:
-            l_masseur[index] = 1
+         if l_elem[index] > n:
+            l_elem[index] = 1
             if index == 0:
-               l_masseur[index] = 0
-            l_masseur[index + 1] = l_masseur[index + 1] + 1
+               l_elem[index] = 0
+            l_elem[index + 1] = l_elem[index + 1] + 1
          index = index + 1
-      l_masseur[0] = l_masseur[0] + 1
+      l_elem[0] = l_elem[0] + 1
       i = i + 1
 
 if __name__ == "__main__":
